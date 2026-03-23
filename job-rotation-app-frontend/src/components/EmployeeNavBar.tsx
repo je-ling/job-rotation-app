ed242bbeb11becae4cad48a04be6b204c790f20d
Jenny <jen.ling4@outlook.com>
2026-03-23 17:30:38 +0000

    refactor: updating toggle menu names and employee navbar

--- job-rotation-app-frontend/src/components/EmployeeNavBar.tsx
+++ job-rotation-app-frontend/src/components/EmployeeNavBar.tsx
@@ -1,10 +1,10 @@
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import logo from '../assets/logo.svg';
+
 
 function EmployeeNavBar() {
   return (
@@ -12,8 +12,17 @@ function EmployeeNavBar() {
       {[false].map((expand) => (
         <Navbar key={String(expand)} expand={expand} className="bg-body-tertiary mb-3">
           <Container fluid>
+
+              <img
+              src={logo}
+              width="150"
+              height="80"
+              alt= "Company Logo"
+              style= {{marginLeft: '20px'}}
+            />
+         
             <Navbar.Brand style={{ flex: 1, textAlign: 'center', fontSize: '2.2rem' }} href="#">Next Step</Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} style={{ marginRight: '20px' }} />
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-${expand}`}
               aria-labelledby={`offcanvasNavbar-Title-${expand}`}
@@ -28,22 +37,23 @@ function EmployeeNavBar() {
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
                   <Nav.Link href="#action1">Home</Nav.Link>
-                  <Nav.Link href="#action2">Link</Nav.Link>
+                  <Nav.Link href="#action2">FAQs</Nav.Link>
                   <NavDropdown
-                    title="Dropdown"
+                    title="Resources"
                     id={`offcanvasNavbarDropdown-expand-${expand}`}
                   >
-                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action4">
-                      Another action
-                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#rotation-process">Rotation Process</NavDropdown.Item>
+                    <NavDropdown.Item href="#express-interest"> Express Interest in a Role </NavDropdown.Item>
+                    <NavDropdown.Item href="#gtd-support"> GTD Support </NavDropdown.Item>
+                    <NavDropdown.Item href="#people-support"> People Support </NavDropdown.Item>
                     <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action5">
-                      Something else here
+                    <NavDropdown.Item href="#staffing-managers-contacts">
+                      Staffing Managers Contact List
                     </NavDropdown.Item>
                   </NavDropdown>
+                  <Nav.Link href="#staffing-manager-login">Staffing Manager Login</Nav.Link>
                 </Nav>
-                <Form className="d-flex">
+                {/* <Form className="d-flex">
                   <Form.Control
                     type="search"
                     placeholder="Search"
@@ -51,7 +61,7 @@ function EmployeeNavBar() {
                     aria-label="Search"
                   />
                   <Button variant="outline-success">Search</Button>
-                </Form>
+                </Form> */}
               </Offcanvas.Body>
             </Navbar.Offcanvas>
           </Container>
