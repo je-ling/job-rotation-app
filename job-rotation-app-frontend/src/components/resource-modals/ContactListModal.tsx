9c4ada8b9b162c534e3e064af324eee7def4a771
Jenny <jen.ling4@outlook.com>
2026-05-21 17:05:05 +0100

    refactor: update emails of staffing managers

--- job-rotation-app-frontend/src/components/resource-modals/ContactListModal.tsx
+++ job-rotation-app-frontend/src/components/resource-modals/ContactListModal.tsx
@@ -13,12 +13,12 @@ const ContactListModal = ({ show, onClose }: ContactListModalProps) => {
             </Modal.Header>
             <Modal.Body>
                 <p><strong>Platform Engineering:</strong> bob.smith@example.com</p>
-                <p><strong>Agile Delivery:</strong> sarah.jane@example.com</p>
-                <p><strong>Engagement Management:</strong> jane.doe@example.com</p>
-                <p><strong>Insights & Data:</strong> emily.clark@example.com and john.doe@example.com</p>
+                <p><strong>Agile Delivery:</strong> john.doe@example.com</p>
+                <p><strong>Engagement Management:</strong> will.smith@example.com</p>
+                <p><strong>Insights & Data:</strong> emily.clark@example.com</p>
                 <p><strong>Architecture:</strong> charlie.brown@example.com</p>
-                <p><strong>Development:</strong> fred.jones@example.com</p>
-                <p><strong>Digital Experience:</strong> will.smith@example.com</p>
+                <p><strong>Development:</strong> fred.jones@example.com and sarah.jane@example.com</p>
+                <p><strong>Digital Experience:</strong> jane.doe@example.comm</p>
                 <p><strong>Business Operations:</strong> debbie.white@example.com</p>
             </Modal.Body>
             <Modal.Footer>
